81524cd16c0b2fa8dfbbc465096f402c253dac29
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-09 22:19:49 +0530

    Serving and Fetching Data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import products from './data/products.js';
+
 const port = 5001;
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('API Running')
+    res.send('API Running');
+});
+
+app.get('/api/products', (req,res) => {
+    res.json(products);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
