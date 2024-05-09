b4f83c4b196c269efb8fa7f5634352d636f596ad
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-09 22:20:14 +0530

    Serving Data

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config(); 
 import products from './data/products.js';
 
-const port = 5001;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.get('/', (req, res) => {
@@ -10,6 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/products', (req,res) => {
     res.json(products);
+});
+
+app.get('/api/products/:id', (req,res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
