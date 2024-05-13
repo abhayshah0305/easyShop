e5506b3bcfcd68b60120aaae2ed40862ee488f6f
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-14 02:24:15 +0530

    Bug Fix

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,23 +7,29 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
-    return (
+  console.log('Products:', products); // Check the structure of product data
+  console.log('Is Loading:', isLoading); // Check if it is still loading
+  console.log('Error:', error); // Check if there's an error
+
+  if (isLoading) return <Loader />;
+
+  if (error) {
+    const errorMessage = error?.data?.message || 'An unexpected error occurred.';
+    return <div>{errorMessage}</div>;
+  }
+
+  if (!products || products.length === 0) return <div>No products found.</div>;
+
+  return (
     <>
-    { isLoading ? (
-      <Loader />
-    ) : error ? (
-    <div>{error?.data?.message || error.error}</div>
-    ) : (
-      <>
       <h1>Latest Products</h1>
       <Row>
-          {products.map((product) => (
-              <Col key = {product._id} sm = {12} md = {6} lg={4} xl={3}>
-                <Product product={product}/>
-              </Col>
-          ))}
+        {products.map(product => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
       </Row>
-      </>)}
     </>
   );
 };
