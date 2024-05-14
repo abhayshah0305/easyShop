da3ef2b1d83f16c96836bd6a691bda8bead7cb4d
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-14 18:36:48 +0530

    Authentication FInal

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
+//import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config(); 
 import cookieParser from 'cookie-parser';
+dotenv.config();
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+//import orderRoutes from './routes/orderRoutes.js';
+//import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-import productRoutes from './routes/productRoutes.js'
 
 const port = process.env.PORT || 5000;
 connectDB();
@@ -21,6 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
