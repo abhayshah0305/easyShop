91ed1ec573dd42e75980b0ef32fc65a190dbcc10
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-14 16:27:59 +0530

    Backend Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config(); 
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -10,6 +11,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API Running');
 });
