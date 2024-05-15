55f5896343f148c4c03d5d0d3a4877f726207d07
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-16 04:29:26 +0530

    Update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -33,8 +33,7 @@ const Header = () => {
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
-              ProShop
+              EasyShop
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
